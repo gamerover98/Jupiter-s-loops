05625ee37147e63122205c8accbff0ed0a29fbf4
gamerover98 <gamerover98@hotmail.it>
2024-03-20 23:12:22 +0100

    Updated ship movements with accelerations

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -2,24 +2,45 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
-    public float speed = 5f;
-    private Rigidbody _rigidBody;
 
+    public Camera mainCamera;
+    
+    public float accelerationY = 5f;
+    public float maxSpeedY = 10f;
+    public float decelerationY = 5f;
+
+    public float speedX = 0.01f;
+    
+    private Rigidbody _rigidBody;
+    private float _initDistance;
+    
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody>();
+        _initDistance = Mathf.Abs(_rigidBody.position.x - mainCamera.transform.position.x);
+    }
+
+    private void Update()
+    {
+        var pos = mainCamera.transform.position;
+        mainCamera.transform.position = new Vector3(_rigidBody.position.x + _initDistance, pos.y, pos.z);
     }
 
     private void FixedUpdate()
     {
         var verticalMovement = Input.GetAxis("Vertical");
-        var horizontalMovement = Input.GetAxis("Horizontal");
+        var newVelocity = _rigidBody.velocity;
+        
+        newVelocity += transform.up * (accelerationY * verticalMovement * Time.fixedDeltaTime);
+        newVelocity.y = Mathf.Clamp(newVelocity.y, -maxSpeedY, maxSpeedY);
 
-        var mx = transform.right * (horizontalMovement * speed * Time.fixedDeltaTime);
-        var my = transform.up * (verticalMovement * speed * Time.fixedDeltaTime);
+        newVelocity += -transform.right * speedX;
+        newVelocity.x = Mathf.Clamp(newVelocity.x, speedX, speedX);
+        
+        if (verticalMovement == 0 && _rigidBody.velocity.magnitude > 0)
+            newVelocity -= _rigidBody.velocity.normalized * (decelerationY * Time.fixedDeltaTime);
 
-        var velocity = new Vector3(mx.x, my.y);
-        _rigidBody.MovePosition(_rigidBody.position + velocity);
+        _rigidBody.MovePosition(_rigidBody.position + newVelocity * Time.fixedDeltaTime);
     }
 
     private void OnCollisionEnter(Collision collision)
