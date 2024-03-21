aa8b654f8131d4ce1a7d105a69c057400fbb0c51
gamerover98 <gamerover98@hotmail.it>
2024-03-21 19:29:49 +0100

    Refactoring Ship script

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -2,18 +2,16 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
-
     public Camera mainCamera;
-    
-    public float accelerationY = 5f;
-    public float maxSpeedY = 10f;
-    public float decelerationY = 5f;
 
-    public float speedX = 0.01f;
-    
+    [SerializeField] private float accelerationY = 5f;
+    [SerializeField] private float maxSpeedY = 10f;
+    [SerializeField] private float decelerationY = 5f;
+    [SerializeField] private float speedX = 0.01f;
+
     private Rigidbody _rigidBody;
     private float _initDistance;
-    
+
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody>();
@@ -22,25 +20,44 @@ public class Ship : MonoBehaviour
 
     private void Update()
     {
-        var pos = mainCamera.transform.position;
-        mainCamera.transform.position = new Vector3(_rigidBody.position.x + _initDistance, pos.y, pos.z);
+        var cameraTransform = mainCamera.transform;
+        var cameraPosition = cameraTransform.position;
+
+        var targetPosition =
+            new Vector3(
+                gameObject.transform.position.x + _initDistance,
+                cameraPosition.y,
+                cameraPosition.z);
+
+        cameraTransform.position = targetPosition;
     }
 
     private void FixedUpdate()
     {
         var verticalMovement = Input.GetAxis("Vertical");
         var newVelocity = _rigidBody.velocity;
-        
-        newVelocity += transform.up * (accelerationY * verticalMovement * Time.fixedDeltaTime);
-        newVelocity.y = Mathf.Clamp(newVelocity.y, -maxSpeedY, maxSpeedY);
 
-        newVelocity += -transform.right * speedX;
-        newVelocity.x = Mathf.Clamp(newVelocity.x, speedX, speedX);
+        MoveVertically(ref newVelocity, verticalMovement);
+        MoveForward(ref newVelocity);
         
-        if (verticalMovement == 0 && _rigidBody.velocity.magnitude > 0)
-            newVelocity -= _rigidBody.velocity.normalized * (decelerationY * Time.fixedDeltaTime);
-
         _rigidBody.MovePosition(_rigidBody.position + newVelocity * Time.fixedDeltaTime);
+    }
+
+    private void MoveForward(ref Vector3 newVelocity)
+    {
+        newVelocity += -transform.right.normalized * speedX; // -right is because the model is rotated by 180 degrees.
+        newVelocity.x = Mathf.Clamp(newVelocity.x, speedX, speedX);
+    }
+
+    private void MoveVertically(ref Vector3 velocity, float verticalMovement)
+    {
+        velocity += transform.up.normalized * (verticalMovement * accelerationY);
+        velocity.y = Mathf.Clamp(velocity.y, -maxSpeedY, maxSpeedY);
+
+        if (verticalMovement == 0 && _rigidBody.velocity.magnitude > 0)
+        {
+            velocity -= new Vector3(0, _rigidBody.velocity.normalized.y * decelerationY);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
