e9dd43d07d0fa2c4bddf07e8a7fc4fc6fc88a55e
SabinoPantaleo <sabino.pantaleo@gmail.com>
2024-05-05 23:36:48 +0200

    added some code for managing the Health container

--- Assets/Scripts/Mono/GUI/GameGUI.cs
+++ Assets/Scripts/Mono/GUI/GameGUI.cs
@@ -1,4 +1,6 @@
-﻿using Api.Common;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Api.Common;
 using TMPro;
 using UnityEngine;
 
@@ -10,10 +12,17 @@ namespace Mono.GUI
 
         [SerializeField] public TextMeshProUGUI countdownText;
         [SerializeField] public TextMeshProUGUI healthText;
+        [SerializeField] public List<GameObject> LifeIcons = new List<GameObject>();
         [SerializeField] public TextMeshProUGUI distanceText;
 
         public void UpdateCountdownText(int startingTime) => countdownText.text = $"Starting in {startingTime} ...";
-        public void UpdateHealth(int value) => healthText.text = $"Health: {value}";
+        //public void UpdateHealth(int value) => healthText.text = $"Health: {value}";
+        public void UpdateHealth(int value)
+        {
+            for (int i = LifeIcons.Count - 1; i >= 0; i--)
+                LifeIcons[i].SetActive(value > i);
+        }
+        
         public void UpdateDistanceText(float value) => 
             distanceText.text = $"{MathUtil.TrimFloat(value, DistanceMantissaPrecision)} m";
         
