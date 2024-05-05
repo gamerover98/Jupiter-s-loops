e75b58bac84dcb002607edff3a4959456a548c4f
SabinoPantaleo <sabino.pantaleo@gmail.com>
2024-05-05 23:23:33 +0200

    Make a custom Pause Menu, HUD with score and HP (need Script)

--- Assets/Scripts/Mono/GUI/GameGUI.cs
+++ Assets/Scripts/Mono/GUI/GameGUI.cs
@@ -6,7 +6,7 @@ namespace Mono.GUI
 {
     public class GameGUI : MonoBehaviour
     {
-        private const int DistanceMantissaPrecision = 10;
+        private const int DistanceMantissaPrecision = 1;
 
         [SerializeField] public TextMeshProUGUI countdownText;
         [SerializeField] public TextMeshProUGUI healthText;
@@ -15,7 +15,7 @@ namespace Mono.GUI
         public void UpdateCountdownText(int startingTime) => countdownText.text = $"Starting in {startingTime} ...";
         public void UpdateHealth(int value) => healthText.text = $"Health: {value}";
         public void UpdateDistanceText(float value) => 
-            distanceText.text = $"Distance: {MathUtil.TrimFloat(value, DistanceMantissaPrecision)}";
+            distanceText.text = $"{MathUtil.TrimFloat(value, DistanceMantissaPrecision)} m";
         
         public void SetActive(bool active) => gameObject.SetActive(active);
         public bool IsActive() => gameObject.activeSelf;
